a34c1f7f73f12b07510fcac0012eee52416ef052
Excalibur202 <pedro.92@icloud.com>
2020-03-31 17:20:27 +0100

    Transfered Files
    Transfered my files (0n0n) to the new project

---
+++ Assets/0n0n/Menu Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartLoad()
+    {
+        StartCoroutine(StartLoading());
+    }
+
+    private IEnumerator StartLoading()
+    {
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(1);
+    }
+
+    void QuitButton()
+    {
+        Application.Quit();
+    }
+}
