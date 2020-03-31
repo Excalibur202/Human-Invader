896cf907f660904e37f0f7c440ed8f5fca313e2a
Kikbow Zutachi <mutecanon@hotmail.com>
2020-03-31 16:43:14 +0100

    Added the terminal and its dependencies (commented out map info features!)

---
+++ Assets/Unorganized Files/Terminal/TerminalCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class TerminalCommand
+{
+    public string desc;
+    public Action action;
+    public string[] examples;
+    public bool hiddenFromList;
+
+    public TerminalCommand(string Description, Action Action, bool HiddenFromList, params string[] Examples)
+    {
+        desc = Description;
+        action = Action;
+        examples = Examples;
+        hiddenFromList = HiddenFromList;
+    }
+
+    public void ExecuteCommand()
+    {
+        action();
+    }
+}
