5e009577275fa9c44875830df0ca66548a75864d
Kikbow Zutachi <mutecanon@hotmail.com>
2020-05-05 00:41:51 +0100

    Added melee enemy initial scripting

---
+++ Assets/Unorganized Files/Enemies/Melee.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Melee : BaseEnemy {
+	enum AggroAction {
+		none,
+		attack,
+		ability1,
+	}
+	AggroAction aggroAction = AggroAction.none;
+
+	[SerializeField] float ability1CooldownMax;
+	float ability1Cooldown;
+	float attackTimer;
+
+	// Start is called before the first frame update
+	void Start () {
+		base.Start ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		UpdateTimers ();
+
+		switch (aggroAction) {
+			case AggroAction.none:
+				UpdateAI ();
+				break;
+
+			case AggroAction.ability1:
+				Ability1 ();
+				break;
+
+			case AggroAction.attack:
+				MeleeAttack ();
+				break;
+		}
+	}
+
+	void UpdateAI () {
+		// Run basic chase and pathfinding AI from base class
+		base.Update ();
+
+		switch (currentBehavior) {
+			case Behavior.aggro:
+				AggroAI ();
+				break;
+		}
+	}
+
+	void AggroAI () {
+
+		if (canSeePlayer) {
+
+			if (ability1Cooldown == 0 && playerSqrDistance < Util.Square (2f)) {
+				ability1Cooldown = ability1CooldownMax;
+
+				aggroAction = AggroAction.ability1;
+			}
+
+			if (playerSqrDistance < Util.Square (1f)) {
+				aggroAction = AggroAction.attack;
+			}
+		}
+	}
+
+	void Ability1 () {
+
+	}
+
+	void MeleeAttack () {
+
+	}
+
+	void UpdateTimers () {
+		if (attackTimer > 0) {
+			attackTimer -= Time.deltaTime;
+			if (attackTimer < 0) {
+				attackTimer = 0;
+			}
+		}
+
+		if (ability1Cooldown > 0) {
+			ability1Cooldown -= Time.deltaTime;
+			if (ability1Cooldown < 0) {
+				ability1Cooldown = 0;
+			}
+		}
+	}
+}
