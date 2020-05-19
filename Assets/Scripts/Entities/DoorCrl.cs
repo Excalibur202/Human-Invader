53fe27660c8fb2aa974db177ef51b992ad78eb4b
NuclearFTW <rruunneeftw@gmail.com>
2020-05-19 15:33:16 +0100

    Door state

--- Assets/Scripts/Entities/DoorCrl.cs
+++ Assets/Scripts/Entities/DoorCrl.cs
@@ -6,15 +6,18 @@ public class DoorCrl : MonoBehaviour
 {
     public Animation WingLeft;
     public Animation WingRight;
-    
+    public bool unlocked = false;
+
     public void OpenDoor()
     {
+        unlocked = true;
         GetComponent<AudioSource>().Play();
 
         WingLeft["door_01_wing_left"].speed = 1;
         WingRight["door_01_wing_right"].speed = 1;
         WingLeft.Play();
         WingRight.Play();
+
     }
 
     public void CloseDoor()
